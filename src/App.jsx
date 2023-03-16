ff52488c5ec5d799f726980b7055399cb3059ad4
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-16 17:09:59 +0100

    code state for quiz start and import relevant components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import StartPage from './components/StartPage'
+import QuestionsPage from './components/QuestionsPage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStart, setGameStart] = useState(false)
+
+  function startGame(){
+    setGameStart(true)
+  }
 
   return (
     <div className="App">
-      hi
+      {!gameStart && <StartPage handleClick={startGame}/>}
+      {gameStart && <QuestionsPage />}
+      <img src="./blob1.png" alt="" className='blob1'/>
+      <img src="./blob2.png" alt="" className='blob2'/>
     </div>
   )
 }
