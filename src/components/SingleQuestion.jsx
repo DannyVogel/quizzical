f7f54bcfd297bd309c2dd9c505a63a34d0e5ce5c
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-17 00:15:38 +0100

    code question component with render for answer button elements

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import AnswerButton from "./AnswerButton";
 
 export default function SingleQuestion(props) {
-  const {question, correctAnswer, incorrectAnswers, type, id } = props.question
-  
+  const [selected, setSelected] = useState()
+  const { question, correctAnswer, allAnswers, id} = props.question;
+
+  function handleAnswerClick(event) {
+    let selectedAnswer = event.target.innerText;
+    setSelected(selectedAnswer);
+  }
+
+  useEffect(() => {
+    props.updateSelectedAnswer(id, selected)
+  },[selected])
+
+  const answerButtonEls = allAnswers.map((answer, index) => {
+    return (
+      <AnswerButton
+        key={index}
+        answer={answer}
+        correctAnswer={correctAnswer}
+        isSelected={selected}
+        handleClick={handleAnswerClick}
+        gameEnd={props.gameEnd}
+      />
+    );
+  });
+
   return (
     <div className="singleQuestionContainer">
-    <h1 className='questionHeader'>{question}</h1>
-    <div className='answerContainer'>
-        <button className="answerBtn btn">{correctAnswer}</button>
-        <button className="answerBtn btn">{incorrectAnswers[0]}</button>
-        <button className="answerBtn btn">{incorrectAnswers[1]}</button>
-        <button className="answerBtn btn">{incorrectAnswers[2]}</button>
+      <h1 className="questionHeader">{question}</h1>
+      <div className="answerContainer">
+        {answerButtonEls}
+      </div>
     </div>
-</div>
-  )
+  );
 }
