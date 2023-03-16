d78c5ae5cd93359aad2390b55733cf2769dc6d8c
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-17 00:09:40 +0100

    create object class constructor with API string decoder

--- src/class.js
+++ src/class.js
@@ -4,15 +4,12 @@ import { v4 as uuidv4 } from 'uuid'
 class Quiz {
     constructor(data){
         this.id = uuidv4()
-        this.type = data.type
         this.question = decode(data.question)
         this.incorrectAnswers = data.incorrect_answers.map(answer => decode(answer))
         this.correctAnswer = decode(data.correct_answer)
-        this.question = decode(data.question)
+        this.allAnswers = this.incorrectAnswers.concat(this.correctAnswer)
+        this.selectedAnswer = ''
     }
-    // setSelectedAnswer(){
-    //     this.selectedAnswer = 
-    // }
 }
 
 export default Quiz
