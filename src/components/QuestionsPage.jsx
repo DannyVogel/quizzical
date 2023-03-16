e27b03e08f8ba0bc633e28d8110d1b1309dab4c4
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-17 00:15:04 +0100

    add check answers function for game end and restart

--- src/components/QuestionsPage.jsx
+++ src/components/QuestionsPage.jsx
@@ -3,8 +3,10 @@ import { useState, useEffect } from 'react'
 import SingleQuestion from './SingleQuestion'
 import Quiz from '../class'
 
-export default function QuestionsPage() {
+export default function QuestionsPage(props) {
   const [fiveQuestions, setFiveQuestions] = useState([])
+  const [gameEnd, setGameEnd] = useState(false)
+  const [result, setResult] = useState(0)
   
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5')
@@ -14,18 +16,44 @@ export default function QuestionsPage() {
       })
   },[])
 
-  const questionElements = fiveQuestions.map(question => {
+  function updateSelectedAnswer(id, answer){
+    setFiveQuestions(prevFive =>{
+      return prevFive.map(question => {
+        return question.id == id ? {...question, selectedAnswer: answer} : question
+      })
+    })
+  }
+
+  function checkAnswers(){
+    const correctlyAnswered = fiveQuestions.filter(question => question.selectedAnswer === question.correctAnswer)
+    const numberCorrect = correctlyAnswered.length
+    setResult(numberCorrect)
+    setGameEnd(true)
+  }
+
+  const singleQuestionElements = fiveQuestions.map(question => {
     return <SingleQuestion 
       key={question.id}
       question={question}
+      updateSelectedAnswer={updateSelectedAnswer}
+      gameEnd={gameEnd}
     />
   })
 
+  console.log(fiveQuestions)
+
   return (
     <div className='allQuestionsContainer'>
-      {questionElements}
-      
-      <button className='questionPageBtn btn'>Check Answers</button>
+      {singleQuestionElements}
+
+      <div className="btnContainer">
+        {!gameEnd && <button className='questionPageBtn btn' onClick={checkAnswers}>Check Answers</button>}
+        {gameEnd &&
+          <div className="resultsContainer">
+            <p className='resultsTxt'>You scored {result}/5 correct answers</p>
+            <button className='questionPageBtn btn' onClick={props.startOver}>Play again</button>
+          </div>}
+      </div>
     </div>
   )
 }
