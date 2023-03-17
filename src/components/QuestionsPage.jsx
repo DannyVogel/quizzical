96f731df66088f76fc8f4e7aa86f9c0020ed2aed
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-17 10:16:21 +0100

    add loading animation during API fetch

--- src/components/QuestionsPage.jsx
+++ src/components/QuestionsPage.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import SingleQuestion from './SingleQuestion'
 import Quiz from '../class'
+import { Triangle } from  'react-loader-spinner'
 
 export default function QuestionsPage(props) {
   const [fiveQuestions, setFiveQuestions] = useState([])
   const [gameEnd, setGameEnd] = useState(false)
   const [result, setResult] = useState(0)
+  const [isLoading, setLoading] = useState(true)
   
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5')
       .then(res => res.json())
       .then(data => {
         setFiveQuestions(data.results.map(quiz => new Quiz(quiz)))
+        setTimeout(() => {
+          setLoading(false)
+        },1000)
       })
   },[])
 
@@ -40,20 +44,31 @@ export default function QuestionsPage(props) {
     />
   })
 
-  console.log(fiveQuestions)
-
   return (
     <div className='allQuestionsContainer'>
-      {singleQuestionElements}
+      {isLoading 
+        && <Triangle
+              height="80"
+              width="80"
+              color="#293264"
+              ariaLabel="triangle-loading"
+              wrapperStyle={{}}
+              wrapperClassName=""
+              visible={true}
+            />
+      }  
+        {!isLoading && singleQuestionElements}
 
-      <div className="btnContainer">
-        {!gameEnd && <button className='questionPageBtn btn' onClick={checkAnswers}>Check Answers</button>}
-        {gameEnd &&
-          <div className="resultsContainer">
-            <p className='resultsTxt'>You scored {result}/5 correct answers</p>
-            <button className='questionPageBtn btn' onClick={props.startOver}>Play again</button>
-          </div>}
+        {!isLoading && 
+          <div className="btnContainer">
+            {!gameEnd && <button className='questionPageBtn btn' onClick={checkAnswers}>Check Answers</button>}
+            {gameEnd &&
+              <div className="resultsContainer">
+                <p className='resultsTxt'>You scored {result}/5 correct answers</p>
+                <button className='questionPageBtn btn' onClick={props.startOver}>Play again</button>
+              </div>}
+          </div>
+        }
       </div>
-    </div>
   )
 }
